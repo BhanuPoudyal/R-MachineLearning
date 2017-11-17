69133a0f901ceeedcc350228a0d65e2ef1a1e587
Magesh <tkmagesh77@gmail.com>
2017-11-17 18:59:57 +0800

    Notes & Code

--- BLACKBOX.R
+++ BLACKBOX.R
@@ -8,13 +8,6 @@ pairs.panels(cancer_data[, c(2,3:10)])
 pairs.panels(cancer_data[, c(2,11:20)])
 pairs.panels(cancer_data[, c(2,21:32)])
 
-#Principal Component Analysis
-#scale the data first
-scaled_data <- scale(cancer_data[, 3:32]) 
-#convert into principal components 
-pca_data <- prcomp(scaled_data)
-plot(pca_data)
-summary(pca_data)
 
 #Get only the first 3 components
 final_data <- data.frame(pca_data$x[,1:3]) 
@@ -29,24 +22,35 @@ dim(training);dim(testing)
 
 table(training$diagnosis); table(testing$diagnosis)
 
+install.packages("car")
+install.packages("nnet")
+install.packages("caTools")
+
+library(car)
+library(nnet)
+library(caTools)
+
+
 predlist <- c("bagFDA","LogitBoost", "nnet", "svmRadialCost") 
 #Create a result data set
 results <- data.frame( Algorithm=character(), Duration=numeric(), Accuracy=numeric(), stringsAsFactors=FALSE)
 #loop through algorithm list and perform model building and prediction
 for (i in 1:length(predlist)) {
-pred <- predlist[i] print(paste("Algorithm = ",pred )) #Measure Time
-startTime <- as.integer(Sys.time())
-#Build model
-model <- train( diagnosis ~ ., data=training, method=pred) #Predict
-predicted <- predict(model, testing)
-#Compare results
-matrix<- confusionMatrix(predicted, testing$diagnosis)
-#Measure end time
-endTime <- as.integer(Sys.time())
-#Store result
-thisresult <- c( as.character(pred), endTime-startTime, as.numeric(matrix$overall[1])) results[i,1] <- pred
-results[i,2] <- endTime-startTime
-results[i,3] <- round(as.numeric(matrix$overall[1]) * 100, 2)
+	pred <- predlist[i] 
+	print(paste("Algorithm = ",pred )) 
+	#Measure Time
+	startTime <- as.integer(Sys.time())
+	#Build model
+	model <- train( diagnosis ~ ., data=training, method=pred) #Predict
+	predicted <- predict(model, testing)
+	#Compare results
+	matrix<- confusionMatrix(predicted, testing$diagnosis)
+	#Measure end time
+	endTime <- as.integer(Sys.time())
+	#Store result
+	thisresult <- c( as.character(pred), endTime-startTime, as.numeric(matrix$overall[1])) results[i,1] <- pred
+	results[i,2] <- endTime-startTime
+	results[i,3] <- round(as.numeric(matrix$overall[1]) * 100, 2)
 }
 
  #Print results
